2d45ed5d38ade17e633aebc65366c43e5d3beb0c
xDaemonic <errewer123@gmail.com>
2025-05-23 21:03:14 +0300

    upload local

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
 
-Route::get('/', [SiteController::class, 'index'])->name('home');
+Route::get('/', function() {
+  dd('ok');
+});
+// Route::get('/', [SiteController::class, 'index'])->name('home');
 Route::get('/history', [SiteController::class, 'history'])->name('history');
 Route::get('/agents', [SiteController::class, 'agents'])->name('agents');
