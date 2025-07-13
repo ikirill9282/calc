cab9cdc73e7a13ed9a0c5d3719843b3463778e64
xDaemonic <errewer123@gmail.com>
2025-07-13 14:33:16 +0300

    upload local

--- app/Console/Commands/LoadSheet.php
+++ app/Console/Commands/LoadSheet.php
@@ -26,18 +26,40 @@ class LoadSheet extends Command
      */
     protected $description = 'Command description';
 
+
+    protected static array $headers = [
+      0 => "wh",
+      1 => "phone",
+      2 => "opened",
+      3 => "wh_address",
+      4 => "map",
+      5 => "distributor",
+      6 => "distributor_center",
+      7 => "distributor_center_delivery_date",
+      8 => "delivery_diff",
+      9 => "delivery_weekend",
+      10 => "pick_diff",
+      11 => "pick_weekend",
+      12 => "pick_tariff_min",
+      13 => "pick_tariff_vol",
+      14 => "pick_tariff_pallete",
+      15 => "pick_additional",
+      16 => "delivery_tariff_min",
+      17 => "delivery_tariff_vol",
+      18 => "delivery_tariff_pallete",
+    ];
+
     /**
      * Execute the console command.
      */
     public function handle()
     {
         $sid = '1ZOkCAKId9W5nAQya3ZFC1GyYeeGM-Mbne7U-F44Zw-E';
-        $attrs = Schema::getColumnListing('sheet_data');
-        $header = array_values(array_filter($attrs, fn($val) => !in_array($val, ['id', 'created_at', 'updated_at'])));
         $rows = Sheets::spreadsheet($sid)->sheet('Лист1')->get();
         $rows->forget([0, 1, 2]);
 
-        $data = Sheets::collection($header, $rows)->toArray();
+        $data = Sheets::collection(static::$headers, $rows)->toArray();
+
         $data = array_map(function($item) {
           $item['distributor_center_delivery_date'] = Carbon::parse($item['distributor_center_delivery_date'])->format('Y-m-d H:i:s');
           $item['delivery_diff'] = Carbon::parse($item['delivery_diff'])->format('Y-m-d H:i:s');
@@ -50,6 +72,7 @@ class LoadSheet extends Command
         try {
           SheetData::where('id', '>', 0)->delete();
           foreach ($data as $item) {
+            unset($item['phone'], $item['opened']);
             SheetData::create($item);
           }
         } catch (\Exception $e) {
