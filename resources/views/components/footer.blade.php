8dd43f25a22d45058beb47fa521a7e6ca3ccdff9
xDaemonic <errewer123@gmail.com>
2025-07-16 14:45:42 +0300

    upload local

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,42 +1,22 @@
 <footer class="px-2 sm:px-5 2xl:px-10 pb-3">
-    <div class="px-6 2xlpx-12 w-full rounded-lg shadow bg-white dark:bg-primary-900">
-        {{-- <div class="grid grid-cols-1 sm:grid-cols-3 gap-6 lg:gap-12 py-6 lg:py-12">
-            <div class="flex flex-col gap-4">
-                <span>E-mail:<x-link href="mailto:{{ env('MAIL_FROM_ADDRESS') }}">{{ env('MAIL_FROM_ADDRESS') }}</x-link></span>
-                <span>Адрес: {{ env('ADDRESS') }}</span>
-            </div>
+    <div class="px-6 2xl:px-12 w-full rounded-lg shadow bg-white dark:bg-primary-900">
+        
+        <div class="grid grid-cols-2 sm:grid-cols-3 gap-12 py-6">
+            <div class="hidden"></div>
+            <div class="hidden"></div>
             <div class="">
-                <nav>
-                    <ul class="flex flex-col justify-start items-stretch gap-4">
-                        <li><x-link>Главная</x-link></li>
-                        <li><x-link>История заказов</x-link></li>
-                        <li><x-link>Контрагенты</x-link></li>
-                    </ul>
-                </nav>
+              <div class="py-6 lg:py-12">
+                <p>ООО ТРАНСПОРТНАЯ КОМПАНИЯ "82 РЕГИОН"</p>
+                <p>ИНН 9102190477</p>
+                <p>ОРГН 1159102108440</p>
+              </div>
             </div>
-            <div class="flex flex-col gap-4">
-                <p>Мы в соц. сетях:</p>
-                <div class="flex gap-4">
-                    <x-link>@include('icons.vk')</x-link>
-                    <x-link>@include('icons.tg')</x-link>
-                </div>
-            </div>
-        </div> --}}
-        <div class="py-6 lg:py-12">
-          <p>ООО ТРАНСПОРТНАЯ КОМПАНИЯ "82 РЕГИОН"</p>
-          <p>ИНН 9102190477</p>
-          <p>ОРГН 1159102108440</p>
         </div>
         <div class="border-b w-full border-primary-600/25"></div>
         <div class="grid grid-cols-2 sm:grid-cols-3 gap-12 py-6">
-            <div class="">
-              {{-- {{ env('APP_NAME', '') }} © {{ date('Y', time()) }} --}}
-              © {{ date('Y', time()) }}
-            </div>
-            <div class="hidden"></div>
-            <div class="hidden">
-              {{-- <x-link>Сделано в {{ env('APP_NAME', '') }}</x-link> --}}
-            </div>
+          <div class="hidden"></div>
+          <div class="hidden"></div>
+          <div class="">© {{ date('Y', time()) }}</div>
         </div>
     </div>
 </footer>
