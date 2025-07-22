fa870993616eed814268423fad725340e44b2871
xDaemonic <errewer123@gmail.com>
2025-07-22 19:31:45 +0300

    upload local

--- resources/views/livewire/success.blade.php
+++ resources/views/livewire/success.blade.php
@@ -131,8 +131,8 @@
                         <td class="py-2 px-4">{{ $row['type'] ?? '' }}</td>
                         <td class="py-2 px-4">@if(isset($row['count'])) {{ $row['count'] }} шт @endif</td>
                         {{-- <td class="py-2 px-4">@if(isset($row['weight'])) {{ $row['weight'] }} кг @endif</td> --}}
-                        <td class="py-2 px-4">@if(isset($row['volume'])) {{ number_format($row['volume'], '', '.') }} м3 @endif</td>
-                        <td class="py-2 px-4">@if(isset($row['cargo']) && (isset($row['count']) || isset($row['weight']) || isset($row['volume']))) {{ $row['cargo'] }} @endif</td>
+                        <td class="py-2 px-4">@if(isset($row['volume'])) {{ number_format($row['volume'], strlen(preg_replace('/^.*?\.(\d+)/is'), "$1", $row['volume']) '.') }} м3 @endif</td>
+                        <td class="py-2 px-4">@if(isset($row['cargo'])) {{ $row['cargo'] }} @endif</td>
                       </tr>                      
                     @endforeach
                   </tbody>
