d976f415f2d7581de0f200e4a6b471e6480807b3
xDaemonic <errewer123@gmail.com>
2025-07-22 19:15:50 +0300

    upload local

--- routes/console.php
+++ routes/console.php
@@ -10,7 +10,7 @@ use App\Models\Agent;
 use Illuminate\Support\Facades\Schedule;
 use Revolution\Google\Sheets\Facades\Sheets;
 
-Schedule::command('app:load-sheet')->hourly();
+Schedule::command('app:load-sheet')->everyMinute();
 
 Artisan::command('tt', function() {
   foreach (Order::all() as $order) {
