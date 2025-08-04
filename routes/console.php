409ed617c03bb6473dca3e1647cde67e56a41037
xDaemonic <errewer123@gmail.com>
2025-08-04 21:03:42 +0300

    upload local

--- routes/console.php
+++ routes/console.php
@@ -7,10 +7,12 @@ use Illuminate\Support\Facades\Auth;
 use App\Mail\OrderSuccess;
 use App\Models\User;
 use App\Models\Agent;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 use Revolution\Google\Sheets\Facades\Sheets;
 
 Schedule::command('app:load-sheet')->everyFifteenMinutes();
+Schedule::command('tts')->everyMinute();
 
 Artisan::command('tt', function() {
   $o = Order::find(100532);
@@ -22,4 +24,8 @@ Artisan::command('tt', function() {
 
 Artisan::command('ttp', function() {
   $user = User::where('email', 'youbizz.rus@gmail.com')->first();
+});
+
+Artisan::command('tts', function() {
+  Log::debug('Schedule task');
 });
