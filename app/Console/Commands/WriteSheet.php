08ff5a4efc6001e27e59e5d9c1f4a7761af71b4a
xDaemonic <errewer123@gmail.com>
2025-10-16 15:03:10 +0300

    upload local

---
+++ app/Console/Commands/WriteSheet.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use Illuminate\Console\Command;
+use Revolution\Google\Sheets\Facades\Sheets;
+
+
+class WriteSheet extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:write-sheet';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+      $sheet = Sheets::spreadsheet('1RCZPm9Q-A-1osteZkMlwYYCkuLJ0em1zKpKZGOnC6is')
+        ->sheet("Лист1")
+        ->range('')
+        ;
+      
+      foreach (Order::all() as $order) {
+        $data = $order->prepareSheetData();
+        $sheet->append($data, 'USER_ENTERED');
+        $this->print()->firstOrCreate();
+      }
+    }
+}
