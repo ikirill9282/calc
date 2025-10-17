896af65cdef1ec238ab57d57de9b9a581e1853da
xDaemonic <errewer123@gmail.com>
2025-10-17 13:31:16 +0300

    upload local

--- routes/console.php
+++ routes/console.php
@@ -15,7 +15,7 @@ use Revolution\Google\Sheets\Facades\Sheets;
 
 if (!env('APP_LOCAL', false)) {
   Schedule::command('app:load-sheet')->everyFifteenMinutes();
-  Schedule::command('app:write-sheet')->everyMinute();
+  Schedule::command('app:write-sheet')->everyFiveMinutes();
 }
 // Schedule::command('tts')->everyMinute();
 
