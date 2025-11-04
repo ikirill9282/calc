9e2d812122edd83037abcd38cee0accb85f7f1e9
Kirill Molochinskiy <kirill9282@MacBook-Pro-Kirill-2.local>
2025-11-04 14:35:58 +0300

    Обновление 042.11.25

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,6 +11,7 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
+use Filament\View\PanelsRenderHook;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -40,6 +41,10 @@ class AdminPanelProvider extends PanelProvider
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
             ])
+            ->renderHook(
+                PanelsRenderHook::HEAD,
+                fn () => view('components.filament-styles'),
+            )
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
---
+++ resources/views/components/filament-styles.blade.php
@@ -0,0 +1,11 @@
+<style>
+    .fi-ta-table thead th,
+    .fi-ta-table tbody td {
+        border-right: 1px solid rgba(148, 163, 184, 0.4);
+    }
+
+    .fi-ta-table thead th:last-child,
+    .fi-ta-table tbody td:last-child {
+        border-right: none;
+    }
+</style>
